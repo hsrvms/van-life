d486cb626d92ae0f3fbd54c9edffdb858c357c51
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-10-27 12:11:26 +0300

    added links

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './Home'
 import About from './About'
 
@@ -8,6 +8,10 @@ function App() {
 
   return (
     <BrowserRouter>
+      <nav>
+        <Link className="link" to="/">Home</Link>
+        <Link className="link" to="/about">About</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
